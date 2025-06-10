28d0c0ada4add04426316b371f385cc2e943a625
Ben Meyer-Meisel <59774249+memben@users.noreply.github.com>
2025-06-11 00:45:35 +0200

    speakeasy run

--- src/zendesk/_version.py
+++ src/zendesk/_version.py
@@ -3,10 +3,10 @@
 import importlib.metadata
 
 __title__: str = "python-zendesk"
-__version__: str = "0.1.0"
+__version__: str = "0.1.1"
 __openapi_doc_version__: str = "2.0.0"
 __gen_version__: str = "2.624.0"
-__user_agent__: str = "speakeasy-sdk/python 0.1.0 2.624.0 2.0.0 python-zendesk"
+__user_agent__: str = "speakeasy-sdk/python 0.1.1 2.624.0 2.0.0 python-zendesk"
 
 try:
     if __package__ is not None:
